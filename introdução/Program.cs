cd6d4429cafabb7e189c1a343185c551e17e3494
edsuu <111332935+edsuuu@users.noreply.github.com>
2023-07-23 22:32:16 -0300

    Primeiro Commit

---
+++ introdução/Program.cs
@@ -0,0 +1,15 @@
+﻿//writeline é para quebrar uma linha no texto
+
+Console.WriteLine("Qual é o seu nome: ");
+string nome = Console.ReadLine()!; //receber dados
+
+Console.WriteLine($" Bem vindo {nome}!!");
+Console.WriteLine("Qual sua idade:");
+
+int idade = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Seu nome é {nome} e sua idade é {idade} anos");
+
+string terminarTela = Console.ReadLine()!;
+
+//ola mundo
