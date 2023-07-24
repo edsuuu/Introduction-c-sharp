65dfa52ec38dbfc7fed4b51a0b36545e4bf50b15
edsuu <111332935+edsuuu@users.noreply.github.com>
2023-07-24 00:01:34 -0300

    att

---
+++ POO-Csharp/novoprojeto/Escola.cs
@@ -0,0 +1,23 @@
+public class Escola
+{
+    public string Aluno;
+    public string Curso;
+    public string Turno;
+    public string Professor;
+    public int Sala;
+
+    public Escola(string aluno, string curso, string turno, string professor, int sala)
+    {
+        Aluno = aluno;
+        Curso = curso;
+        Turno = turno;
+        Professor = professor;
+        Sala = sala;
+    }
+
+    public override string ToString()
+    {
+        return $"Aluno: {Aluno}, Curso: {Curso}, Turno: {Turno}, Professor: {Professor}, Sala: {Sala}";
+    }
+
+}
--- POO-Csharp/novoprojeto/Program.cs
+++ POO-Csharp/novoprojeto/Program.cs
@@ -3,34 +3,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Escola escola = new Escola("Edson", "Tecnologia da Informação", "Manha","João", 1);
+
+        Console.WriteLine("\n" +escola.ToString() + "\n");
+
+        // Console.WriteLine("\n" + escola.Aluno);
+        // Console.WriteLine(escola.Curso);
+        // Console.WriteLine(escola.Turno);
+        // Console.WriteLine(escola.Professor);
+        // Console.WriteLine(escola.Sala + "\n");
     }
 }
+/* string aluno, string curso, string turno, string professor, int sala */
 
-// public class Caneta 
-// {
-//     public string cor;
-
-//     public double Carga;
-//     //double é um tipo de variavel que pode receber numeros decimais '1,50' numeros quebrados
-//     public bool Tampada;
-//     //bool é um tipo de variavel que pode receber 2 valores, verdadeiro ou falso
-//     //sempre inicia com letra minuscula
-// }
-
-public class Escola
+public class Caneta 
 {
-    public string Aluno;
-    //nome do aluno
-    public string Curso;
-    //tipo de curso
-    public string Turno;
-    //manha tarde noite 
-    public string Professor;
-    //nome do professor
-    public int Sala;
-    //int é para receber um numero inteiro "double para usar numero quebrado por exeplo 1.5"
+    public string cor;
+
+    public double Carga;
+    //double é um tipo de variavel que pode receber numeros decimais '1,50' numeros quebrados
+    public bool Tampada;
+    //bool é um tipo de variavel que pode receber 2 valores, verdadeiro ou falso
+    //sempre inicia com letra minuscula
 }
+
+// public class Escola
+// {
+//     public string Aluno;
+//     //nome do aluno
+//     public string Curso;
+//     //tipo de curso
+//     public string Turno;
+//     //manha tarde noite 
+//     public string Professor;
+//     //nome do professor
+//     public int Sala;
+//     //int é para receber um numero inteiro "double para usar numero quebrado por exeplo 1.5"
+// }
 
 public class PessoaClasse
 {
@@ -40,3 +49,7 @@ public class PessoaClasse
 }
 
 //apenas os atributos da classe podem ter ; e a classe finaliza apenas com a {}
+
+
+
+
