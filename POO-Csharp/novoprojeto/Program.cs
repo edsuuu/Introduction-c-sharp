f012f71d68cd82feecd83e32568936c9f34938d2
edsuu <111332935+edsuuu@users.noreply.github.com>
2023-07-23 23:08:51 -0300

    att

---
+++ POO-Csharp/novoprojeto/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace novoprojeto;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+// public class Caneta 
+// {
+//     public string cor;
+
+//     public double Carga;
+//     //double é um tipo de variavel que pode receber numeros decimais '1,50' numeros quebrados
+//     public bool Tampada;
+//     //bool é um tipo de variavel que pode receber 2 valores, verdadeiro ou falso
+//     //sempre inicia com letra minuscula
+// }
+
+public class Escola
+{
+    public string Aluno;
+    //nome do aluno
+    public string Curso;
+    //tipo de curso
+    public string Turno;
+    //manha tarde noite 
+    public string Professor;
+    //nome do professor
+    public int Sala;
+    //int é para receber um numero inteiro "double para usar numero quebrado por exeplo 1.5"
+}
+
+public class PessoaClasse
+{
+    public string Nome;
+    public int Idade;
+    public string Sexo;
+}
+
+//apenas os atributos da classe podem ter ; e a classe finaliza apenas com a {}
